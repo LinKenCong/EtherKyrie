85a6b3df667e3bd1cd814022a053a7a28dab4cf6
kyrie-ap-linux <linkencong@foxmail.com>
2022-03-29 15:49:39 +0800

    Add input validation

--- hardhat/contracts/Voting.sol
+++ hardhat/contracts/Voting.sol
@@ -21,7 +21,7 @@ contract Voting is Initializable {
         bool voted;
     }
     // 计算选票数量
-    uint256 private countResult = 0;
+    uint256 private _countResult = 0;
     // 最终投票数量
     uint256 public finalResult = 0;
     // 总投票人数
@@ -36,9 +36,9 @@ contract Voting is Initializable {
     Proposal[] public proposals;
 
     // 映射 地址 选民
-    mapping(address => Voter) public voterRegister;
+    mapping(address => Voter) private _voterRegister;
     // 映射 地址 提案id=>投票人
-    mapping(uint8 => ProposalVote[]) public voterProposal;
+    mapping(uint8 => ProposalVote[]) private _voterProposal;
     // 赢家
     uint8[] public winnerProposal;
     // 创建枚举 状态{创建，投票，结束}
@@ -72,14 +72,14 @@ contract Voting is Initializable {
 
     modifier voteTrue() {
         // 判断是否投过票
-        require(!voterRegister[msg.sender].voted, "you have already voted.");
+        require(!_voterRegister[msg.sender].voted, "you have already voted.");
         _;
     }
 
     modifier voterTrue() {
         // 判断是否有权限的投票者
         require(
-            bytes(voterRegister[msg.sender].voterName).length != 0,
+            bytes(_voterRegister[msg.sender].voterName).length != 0,
             "You do not have permission."
         );
         _;
@@ -114,8 +114,8 @@ contract Voting is Initializable {
         /// @notice 创建提案
         /// @dev 只有在投票未开始时 只有官方 可执行
         require(bytes(_proposalName).length != 0, "input is empty");
-        Proposal memory newProposal = Proposal(_proposalName);
-        proposals.push(newProposal);
+        Proposal memory _newProposal = Proposal(_proposalName);
+        proposals.push(_newProposal);
     }
 
     function addVoter(address _voterAddress, string memory _voterName)
@@ -127,10 +127,10 @@ contract Voting is Initializable {
         /// @dev 只有在投票未开始时 只有官方 可执行
         require(_voterAddress != address(0), "address is empty");
         require(bytes(_voterName).length != 0, "input is empty");
-        Voter memory v;
-        v.voterName = _voterName;
-        v.voted = false;
-        voterRegister[_voterAddress] = v;
+        Voter memory _v;
+        _v.voterName = _voterName;
+        _v.voted = false;
+        _voterRegister[_voterAddress] = _v;
         totalVoter++;
     }
 
@@ -150,23 +150,23 @@ contract Voting is Initializable {
         /// @notice 投票
         /// @dev 只有在投票开始时执行
         require(_proposalId >= 0, "input is empty");
-        bool found = false;
+        bool _found = false;
 
         // 如果 选民名称不为空
         // 更改状态为已投票
-        voterRegister[msg.sender].voted = true;
+        _voterRegister[msg.sender].voted = true;
         // 加入已被投票的提案
-        ProposalVote memory pv;
-        pv.voteAddress = msg.sender;
-        pv.voteId = _proposalId;
-        voterProposal[_proposalId].push(pv);
+        ProposalVote memory _pv;
+        _pv.voteAddress = msg.sender;
+        _pv.voteId = _proposalId;
+        _voterProposal[_proposalId].push(_pv);
         // 投票数量增加
-        countResult++;
+        _countResult++;
         // 投票人数增加
         totalVote++;
-        found = true;
+        _found = true;
 
-        return found;
+        return _found;
     }
 
     function endVote() public inState(State.Voting) onlyOfficial {
@@ -175,20 +175,20 @@ contract Voting is Initializable {
         // 状态为结束
         state = State.Ended;
         // 最终投票数量显示
-        finalResult = countResult;
+        finalResult = _countResult;
         for (uint8 i = 0; i < proposals.length; i++) {
             if (winnerProposal.length == 0) {
                 // 如果没有赢家则加入
                 winnerProposal.push(i);
             } else if (
-                voterProposal[i].length <
-                voterProposal[winnerProposal[0]].length
+                _voterProposal[i].length <
+                _voterProposal[winnerProposal[0]].length
             ) {
                 // 如果有更小的则变为最小的
                 winnerProposal = [i];
             } else if (
-                voterProposal[i].length ==
-                voterProposal[winnerProposal[0]].length
+                _voterProposal[i].length ==
+                _voterProposal[winnerProposal[0]].length
             ) {
                 // 如果有相同的则加入
                 winnerProposal.push(i);
