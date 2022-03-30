1f00327a380950637ff475d960204f4bc2f39d65
kyrie-ap-linux <linkencong@foxmail.com>
2022-03-30 14:04:43 +0800

    Change Contract the reference structure

--- hardhat/contracts/TopData.sol
+++ hardhat/contracts/TopData.sol
@@ -2,14 +2,12 @@
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+// import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+// import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract TopData is
     OwnableUpgradeable,
-    ERC1155Upgradeable,
-    AccessControlUpgradeable,
-    Initializable
+    ERC1155Upgradeable
 {
     function __TopData_init() internal onlyInitializing {
         __Ownable_init();
