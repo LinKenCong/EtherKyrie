92590b61cc2169adb36bf973d2e6817caf97160c
kyrie-ap-linux <linkencong@foxmail.com>
2022-04-01 18:06:42 +0800

    updata import

--- hardhat/contracts/KyrieEther.sol
+++ hardhat/contracts/KyrieEther.sol
@@ -4,18 +4,10 @@ pragma solidity ^0.8.7;
 import "./PlayersContract.sol";
 
 contract KyrieEther is PlayersContract {
-    uint256 public constant GOLD = 0;
-
     function initializeContract() public initializer {
         __TopContract_init();
         __PlayersContract_init();
         // 初始为合约创建者铸造货币
         _mint(msg.sender, GOLD, 100 * 10**18, "");
-    }
-
-    function mintGoldCoin(address to, uint256 amount) public {
-        require(to != address(0), "ERC1155: mint to the zero address");
-
-        _mint(to, GOLD, amount, "");
     }
 }
--- hardhat/contracts/TopContract.sol
+++ hardhat/contracts/TopContract.sol
@@ -2,17 +2,24 @@
 pragma solidity ^0.8.7;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+
 // import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 // import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract TopContract is
-    OwnableUpgradeable,
-    ERC1155Upgradeable
-{
+contract TopContract is OwnableUpgradeable, ERC1155Upgradeable,SafeMathUpgradeable {
+    uint256 public constant GOLD = 0;
+
     function __TopContract_init() internal onlyInitializing {
         __Ownable_init();
         __ERC1155_init(
             "https://api.frank.hk/api/nft/demo/1155/marvel/{id}.json"
         );
+    }
+
+    function mintGoldCoin(address to, uint256 amount) internal {
+        require(to != address(0), "ERC1155: mint to the zero address");
+
+        _mint(to, GOLD, amount, "");
     }
 }
