db6088d2e50919e4391302fc78cfd91c0159304a
kyrie-ap-linux <linkencong@foxmail.com>
2022-04-02 16:24:43 +0800

    add ERC1155Holder

--- hardhat/contracts/TopContract.sol
+++ hardhat/contracts/TopContract.sol
@@ -3,11 +3,17 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/utils/ERC1155HolderUpgradeable.sol";
 
 // import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 // import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract TopContract is OwnableUpgradeable, ERC1155Upgradeable,SafeMathUpgradeable {
+contract TopContract is
+    OwnableUpgradeable,
+    ERC1155Upgradeable,
+    ERC1155HolderUpgradeable
+{
+    using SafeMathUpgradeable for uint256;
     uint256 public constant GOLD = 0;
 
     function __TopContract_init() internal onlyInitializing {
@@ -15,6 +21,10 @@ contract TopContract is OwnableUpgradeable, ERC1155Upgradeable,SafeMathUpgradeab
         __ERC1155_init(
             "https://api.frank.hk/api/nft/demo/1155/marvel/{id}.json"
         );
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155Upgradeable,ERC1155ReceiverUpgradeable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
     function mintGoldCoin(address to, uint256 amount) internal {
