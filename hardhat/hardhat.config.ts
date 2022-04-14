2d975d2596d96c268389dcb499b06cf424fef584
kyrie-ap-linux <linkencong@foxmail.com>
2022-04-14 17:08:07 +0800

    add optimizer

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -25,7 +25,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config = {
-  solidity: "0.8.7",
+  solidity: "0.8.13",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
